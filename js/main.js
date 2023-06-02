7cee3175172da51adaac4263008bf1c146ed2c73
Augusto Barbosa <augusto.barbosa.dev@gmail.com>
2023-06-02 16:50:44 -0300

    Armazenando elementos no localStorage

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
-const form = (document.getElementById("novoItem"))
+const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
+const itens =  JSON.parse(localStorage.getItem("itens")) || []
+
+itens.forEach( (elemento) => {
+    console.log(elemento.nome, elemento.quantidade)
+})
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+
+    criaElemento(nome.value, quantidade.value)
+    nome.value = ""
+    quantidade.value = ""
 })
 
 function criaElemento(nome, quantidade) {
@@ -19,5 +29,14 @@ function criaElemento(nome, quantidade) {
      novoItem.innerHTML += nome
      
      lista.appendChild(novoItem)
+
+     const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+     }
+
+     itens.push(itemAtual)
+
+     localStorage.setItem("itens", JSON.stringify(itens))
 
 }
